454870a9e88e5c32a8f6c73c47b4d50433ff4b6f
khanadian <iqbal.52@hotmail.com>
2019-01-08 23:41:10 -0500

    the game now has win conditions

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Game 
@@ -6,15 +7,15 @@ public class Game
 	private Tile[] tileList = new Tile[10];
 	private ArrayList<Integer> freeTiles;
 	private ArrayList<Tile[]> winningCombos1; // used to check for win states for player 1
-	private ArrayList<Tile[]> winningCombos2; // used to check for win states for player 2
+	private ArrayList<Tile[]> winningCombos2; // used to check for win states for player 2                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
 	Scanner sc = new Scanner(System.in);
 	private int playerTurn; // indicates which player is playing
 	private boolean play;
 	private boolean isOver;
-	int i;
+	int inp;
 	boolean verified;
 	String input;
-
+	int check;
 	
 	
 	public static void main(String[] args) 
@@ -26,21 +27,25 @@ public class Game
 			}});
 	}
 	
+	
 	private Game()
 	{
 		//setting up
-		for(int i = 1; i < 10; i++)
-			tileList[i] = new Tile();
-		
 		play = true;
 		
 		while(play)
 		{
 			//starting new game
-			setupList(winningCombos1);
-			setupList(winningCombos2);
+			
+			for(int i = 1; i < 10; i++)
+				tileList[i] = new Tile();
+			
+			winningCombos1 = setupList();
+			winningCombos2 = setupList();
 			playerTurn = 1;
 			isOver = false;
+			
+			
 			
 			freeTiles = new ArrayList<Integer>();
 			for(int i = 1; i < 10; i++)
@@ -57,11 +62,11 @@ public class Game
 					try 
 					{
 						input = sc.next();
-						i = Integer.parseInt(input);
+						inp = Integer.parseInt(input);
 						
-						if (i < 1 || i > 9)
+						if (inp < 1 || inp > 9)
 							System.out.println("thats not between 1-9");
-						else if ((tileList[i].isEmpty()) == false)
+						else if ((tileList[inp].isEmpty()) == false)
 							System.out.println("that tile's already taken");
 						else	
 							verified = true;
@@ -70,19 +75,34 @@ public class Game
 					}
 					catch(Exception e){System.out.println("thats not a number");}
 				}
-				//change the free tiles list
-				freeTiles.set(i-1, 0);
 				
-				tileList[i].setSymbol(playerTurn);
+				//player has chosen their move
+				freeTiles.set(inp-1, 0);
+				tileList[inp].setSymbol(playerTurn);
+				
+				if(playerTurn == 1)
+				{
+					removeVoidCombos(winningCombos2, "X"); //error occurs at this line
+					check = checkEnd(winningCombos2);
+				}
+				else
+				{
+					removeVoidCombos(winningCombos1, "O");
+					check = checkEnd(winningCombos1);
+				}
 				
 				
-				playerTurn++;
-				playerTurn = playerTurn % 2; // fancy way of alternating between players
+				if(check == 0)
+				{
+					playerTurn++;
+					playerTurn = playerTurn % 2; // fancy way of alternating between players
+				}
+				else
+					isOver = true;
 			}
+			System.out.println("Game Over!");
+			System.out.println("Player "+ playerTurn + " wins!");
 		}
-		
-		
-		
 		
 	}
 	
@@ -90,9 +110,9 @@ public class Game
 	 * sets up the winning combos for a player
 	 * input: list of winning combinations
 	 */
-	private void setupList(ArrayList<Tile[]> combos)
+	private ArrayList<Tile[]> setupList()
 	{
-		combos = new ArrayList<Tile[]>();
+		ArrayList<Tile[]> combos = new ArrayList<Tile[]>();
 		combos.add(new Tile[] {tileList[1], tileList[2], tileList[3]});
 		combos.add(new Tile[] {tileList[4], tileList[5], tileList[6]});
 		combos.add(new Tile[] {tileList[7], tileList[8], tileList[9]});
@@ -101,6 +121,7 @@ public class Game
 		combos.add(new Tile[] {tileList[3], tileList[6], tileList[9]});
 		combos.add(new Tile[] {tileList[1], tileList[5], tileList[9]});
 		combos.add(new Tile[] {tileList[3], tileList[5], tileList[7]});
+		return combos;
 	}
 	
 	// displays the board
@@ -142,7 +163,47 @@ public class Game
 		return nums;
 	}
 	
+	//checks if game has ended, 1 = win, 2 = draw
+	private int checkEnd(ArrayList<Tile[]> array)
+	{
+		if(winningCombos1.isEmpty() && winningCombos2.isEmpty())
+			return 2;
+		
+		for (Tile[] combos: array)
+		{
+			if(combos[0].getSymbol() != 2 && (combos[0].getSymbol() == combos[1].getSymbol()) && 
+					combos[1].getSymbol() == combos[2].getSymbol())
+			{
+				return 1;
+			}
+		}
+		return 0;
+	}
+	
+	private void removeVoidCombos(ArrayList<Tile[]> array, String symb)
+	{
+		boolean skip = true;
+		List<Integer> toRemove = new ArrayList<Integer>();
+		for (Tile[] combos : array)
+		{
+			skip = true;
+			for(int i = 0; i < 3; i++)
+			{
+				if(combos[i].getSymbText().equals(symb) && skip == true)
+				{
+					toRemove.add(array.indexOf(combos));
+					skip = false;
+				}
+			}
+		}
+		for(Integer i : toRemove) {
+			array.remove(i);
+		}
+	}
 }
+
+// checks if the game has ended. 1 = current player won, 2 = draw, 0 = no ending
+
 
 /*
  - |   |\ /
