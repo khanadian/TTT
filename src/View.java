f17e311ec5469829d711eb8faef8dac834edc0e4
khanadian <iqbal.52@hotmail.com>
2019-01-21 21:00:35 -0500

    made a very simple view

--- src/View.java
+++ src/View.java
@@ -1,24 +1,49 @@
 //import java.awt.event.ActionListener;
 
 import javax.swing.*;
+import java.awt.*;
 
-//private static final long serialVersionUID = 1L;   // just in case
+  
 
 public class View extends JFrame {//implements ActionListener {
 
+	//private static final long serialVersionUID = 1L;  // just in case
+	
 	private Game game;
 	
 	// different panels for different parts of the screen
 	
 	
-	public void main (String[] args)
+	public static void main (String[] args)
 	{
-		JPanel jPan; // haha sounds like japan
-		JFrame frame;
+		JFrame frame = new JFrame("Tic Tac Toe");
+		frame.setLayout(new BorderLayout());
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		
+		JPanel jPan = new JPanel(new GridLayout(3,3));
+		
+		
+		JLabel instructions = new JLabel("X", SwingConstants.CENTER);
+		
+		for(int i = 1; i < 10; i++)
+		{
+			JButton tile = new JButton(Integer.toString(i));
+			tile.setPreferredSize(new Dimension(100, 100));
+			jPan.add(tile);
+		}
+		
+		
+		frame.add(jPan, BorderLayout.CENTER);
+		frame.add(instructions, BorderLayout.NORTH);
+		
+		
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
 		
 	}
+	
 	
 	
 }
